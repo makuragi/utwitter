d04f5092de26a41e61846b172bfc5b54beb7a5fa
kiryo <shirou@shirou.local>
2015-12-23 14:42:38 +0900

    no message

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,7 +6,8 @@ require_once '../include/common/function.php';
 session_start();
 
 // セッション内容を初期化
-if (isSessionExist('user_name'))     $_SESSION['user_name']     = '';
+if (isSessionExist('user_id'))     $_SESSION['user_id']     = '';
+if (isSessionExist('user_name'))    $_SESSION['user_name']    = '';
 if (isSessionExist('user_email'))    $_SESSION['user_email']    = '';
 if (isSessionExist('user_password')) $_SESSION['user_password'] = '';
 if (isSessionExist('user_age'))      $_SESSION['user_age']      = '';
--- htdocs/user_controller.php
+++ htdocs/user_controller.php
@@ -13,13 +13,35 @@ if (!isPost()) {
 	include_once '../include/view/user_create.php';
 } else if (getPost('action_id') === 'user_create_confirm') {
 	// todo 入力チェック
-	$_SESSION['user_name']     = getPost('user_name');
+
+	$_SESSION['user_id']     = getPost('user_id');
+/*    if (!isExist($_SESSION['user_name'])) {
+        $errors[] = 'ユーザネームを入力してください';
+    }
+    if (!isOverText($_SESSION['user_name']), 20) {
+        $errors[] = '文字数は'$maxlen'文字以内にしてください';
+    if ()
+  */      
+        
+//  if (!isUsedID($_SESSION['user_name'])) {
+  //  $errors[] = '既に存在するIDです';
+        
+    $_SESSION['user_name']     = getPost('user_name');
+    // 存在チェック
+/*    if (!isExist($_SESSION['user_name'])) {
+        $errors[] = 'ユーザネームを入力してください';
+    }
+    if (!isOverText($_SESSION['user_name']), 20) {
+        $errors[] = '文字数は２０文字以内にしてください';
+    }*/
 	$_SESSION['user_email']    = getPost('user_email');
 	$_SESSION['user_password'] = getPost('user_password');
 	$_SESSION['user_age']      = getPost('user_age');
 	$_SESSION['user_gender']   = getPost('user_gender');
 	$_SESSION['user_profile']  = getPost('user_profile');
 	// todo 画像アップロッド処理
+
+    
 	include_once '../include/view/user_create_confirm.php';
 }
 if (count($errors) !== 0) {
@@ -27,3 +49,7 @@ if (count($errors) !== 0) {
 		print $error;
 	}
 }
+    
+    
+    // todo 未入力項目のチェック
+    //hush化
