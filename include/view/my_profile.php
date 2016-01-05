4b864c637378732b3ad6b5c90a68ef69e3cee8a2
kent <kento.0109k@gmail.com>
2016-01-05 23:51:20 +0900

    課題#2:プロフィール画面でのフォロー、フォロワー一覧のバグに対応

--- include/view/my_profile.php
+++ include/view/my_profile.php
@@ -16,8 +16,8 @@
 			<div class="my_profile_info clear">
 			<ul class="my_profile_num">
 				<li><p>うついーと数</p><a href="./profile_controller.php?action_id=profile&user_profile_id=<?php echo $user_profile_id; ?>"><?php echo $my_utweet_num; ?></a></li>
-				<li><p>フォロー数</p><a href="./main_controller.php?action_id=follow"><?php echo $my_follow_num; ?></a></li>
-				<li><p>フォロワー数</p><a href="./main_controller.php?action_id=follower"><?php echo $my_follower_num; ?></a></li>
+				<li><p>フォロー数</p><a href="./profile_controller.php?action_id=follow&user_profile_id=<?php echo $user_profile_id; ?>"><?php echo $my_follow_num; ?></a></li>
+				<li><p>フォロワー数</p><a href="./profile_controller.php?action_id=follower&user_profile_id=<?php echo $user_profile_id; ?>"><?php echo $my_follower_num; ?></a></li>
 			</ul>
 			</div>
 			<div class="my_profile_user_profile_mypage">
