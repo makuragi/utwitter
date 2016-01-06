dbf659f48d88643c23c3c940c11fd28d998b9036
kiryo <shirou@shirou.local>
2016-01-06 11:13:37 +0900

    [css更新]メイン画面ボタン表示

--- include/view/post.php
+++ include/view/post.php
@@ -1,7 +1,7 @@
 <div class="right_container">
-	<div id = "post">
+	<div id = "post" class="clearfix">
 	<form action="./main_controller.php" method="post">
-		<textarea name="post_body" rows="4" cols="35" placeholder="何かあった？うつなことをかきこんで、気分を右から選んでね"></textarea>
+		<textarea id="post" name="post_body" rows="4" cols="35" placeholder="何かあった？うつなことをかきこんで、気分を右から選んでね"></textarea>
 		<input type="hidden" name="action_id" value="post_create">
 		<input id="utu_submit" class="hvr-hang" type="image" src="../include/img/parts/utu_submit.png" name="color_id" value="1">
 		<input id="utu_submit" class="hvr-wobble-vertical" type="image" src="../include/img/parts/oko_submit.png" name="color_id" value="2">
